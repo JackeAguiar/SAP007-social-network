ddb478a7f451758d1613b2af3aa22530fd2319f7
Brulibra <brulibra03@hotmail.com>
2022-04-21 17:08:06 -0300

    validação email começo

--- src/pages/home/cadastro.js
+++ src/pages/home/cadastro.js
@@ -10,6 +10,7 @@ export default () => {
 
   <label for="e-mail">E-mail:</label>
   <input type"text" id="email" class="inputs-log" placeholder="exemplo@gmail.com"></input>
+  <p>E-mail inválido</p>
 
   <label for="senha">Senha:</label>
   <input type="password" id="senha" class="inputs-log" placeholder="Digite uma senha de 6 a 8 dígitos"></input>
@@ -17,7 +18,7 @@ export default () => {
   <label for="check-Senha">Confirmar senha:</label>
   <input type="password" id="checkSenha" class="inputs-log" placeholder="Digite novamente sua senha"></input>
   
-  <button class="botaoCadastro" id="botaoCadastro">Cadastrar</button>
+  <button class="botaoCadastro" id="emailRequireError" id="botaoCadastro">Cadastrar</button>
   `;
 
   container.innerHTML = template;
@@ -32,6 +33,18 @@ export default () => {
 
   })
 
+  function emailBlockingButton() {
+    const emailError = container.querySelector("#emailRequireError").value
+    if (!emailError) {
+      document.querySelector("#emailRequireError").style.display = "block"
+    }
+  }
+  emailBlockingButton()
+
+  function validateEmail(email) {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  }
 
   return container;
 };
