cf709728517e8940ad0f524e7207139f2e479939
JackeAguiar <jackelineaguiar000@gmail.com>
2022-04-25 13:49:14 -0300

    tag form adicionada no js

--- src/pages/home/login.js
+++ src/pages/home/login.js
@@ -3,16 +3,18 @@ export default () => {
   container.classList.add('containerLogin');
 
   const templateLogin = `
+       <form>
       <label for="email">Email:</label>
-      <input type="email" placeholder="exemple@gmail.com" id="email" class="email inputs-log"></input>
+      <input type="email" placeholder="exemple@gmail.com" id="email" class="email inputs-log" required></input>
     
       <label for="password">Senha:</label>
-      <input type="password" placeholder="Digite uma senha de 6 a 8 digitos" maxlength="8" id="password" class="password inputs-log"></input>
+      <input type="password" placeholder="Digite uma senha de 6 a 8 digitos" maxlength="8" id="password" class="password inputs-log" required></input>
 
       <button class="button-login" id="button-login">Log-In</button>
       <a href="/#cadastro">
       <button class="button-register" id="button-register">Cadastrar-se</button>
       </a>
+       </form>
     `;
   container.innerHTML = templateLogin;
   return container;
