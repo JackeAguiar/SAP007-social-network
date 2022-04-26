9b8db67956d442259a16237728c619c17db0b6ce
JackeAguiar <jackelineaguiar000@gmail.com>
2022-04-26 19:57:23 -0300

    primeiras alterações do fire-auth

--- src/pages/firebase/auth-firebase.js
+++ src/pages/firebase/auth-firebase.js
@@ -1,19 +1,21 @@
-// import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
 
-// //export function userRegister(email, password) 
+const auth = getAuth();
 
-// export const auth = getAuth();
-// createUserWithEmailAndPassword(auth, email, password)
-//   .then((userCredential) => {
-//     // Signed in
-//     const user = userCredential.user;
-//     // ...
-//     console.log(user)
-//   })
-//   .catch((error) => {
-//     const errorCode = error.code;
-//     const errorMessage = error.message;
-//     console.log("errorCode", errorCode)
-//     console.log("errorMsg", errorMessage)
-//     // ..
-//   });
+export function userRegister(email, password) {
+ return createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed in
+            const user = userCredential.user;
+            // ...
+            console.log(user)
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log("errorCode", errorCode)
+            console.log("errorMsg", errorMessage)
+            // ..
+        });
+
+}
