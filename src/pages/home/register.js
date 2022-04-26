e10e5d4eace859e97ddc292e878b389d82b809cc
ThamaraBortolin <thamaralbortolin@gmail.com>
2022-04-26 20:03:32 -0300

    firebase init

--- src/pages/home/register.js
+++ src/pages/home/register.js
@@ -10,7 +10,7 @@ export default () => {
   <input type="text" id="name" class="inputs-log" placeholder="Nome de usuário" required></input>
 
   <label for="e-mail">E-mail:</label>
-  <input type"email" id="emailRegister" onchance="emailBlockingButton()" class="inputs-log" placeholder="exemplo@gmail.com" required></input>
+  <input type"email" id="emailRegister" class="inputs-log" placeholder="exemplo@gmail.com" required></input>
 
   <label for="senha">Senha:</label>
   <input type="password" id="password" class="inputs-log" placeholder="Digite uma senha de 6 a 8 dígitos" required></input>
@@ -18,7 +18,8 @@ export default () => {
   <label for="check-Senha">Confirmar senha:</label>
   <input type="password" id="checkPassword" class="inputs-log" placeholder="Digite novamente sua senha" required></input>
   
-  <button class="button-register-cas" id="buttonRegister" >Cadastrar</button>
+  <button class="button-register-cas" id="buttonRegister" disabled="disabled" type="button">Cadastrar</button>
+  </form>
   `;
 
   container.innerHTML = template;
@@ -33,23 +34,63 @@ export default () => {
 
   // })
 
-  function emailBlockingButton() {
-    const email = container.querySelector("#emailRegister").value
-    if (!email) {
-      document.querySelector("#buttonRegister").disabled = true
-    }else if (validateEmail(email)){
-      document.querySelector("#buttonRegister").disabled = false
+  const inputs = container.querySelector("#emailRegister");
+  const button = container.querySelector("#buttonRegister");
+  const email = container.querySelector("#emailRegister");
+
+
+  function checkInputs(inputs) {
+    const filled = true;
+    if (inputs.value === "") {
+      filled = false;
     }
-    else{
-      document.querySelector("#buttonRegister").disabled = true
-    }
-  }
-  emailBlockingButton()
+    return filled;
+  };
 
   function validateEmail(email) {
     var re = /\S+@\S+\.\S+/;
-    return re.test(email);
-  }
+    if(re.test(email)){
+      return false
+    }else{
+      return true
+    }
+   }
+
+  inputs.addEventListener("change", function () {
+    if (checkInputs(inputs) && validateEmail(email)) {
+      button.disabled = false;    
+    } else {
+      button.disabled = true;
+  }})
+
+  
+  // const inpEmail = container.querySelector("#emailRegister")
+  // inpEmail.addEventListener("keypress",emailBlockingButton())
+
+  // function emailBlockingButton() {
+  //   // const emailValid = isEmailValid();
+  //   // container.querySelector("#buttonRegister").disabled = !emailValid;
+  //   const email = container.querySelector("#emailRegister").value
+
+  //   if(email === ""){
+  //     container.querySelector("#buttonRegister").classList.add("false")
+  //     console.log('false')
+  //   }else{
+  //     container.querySelector("#buttonRegister").classList.add("true")
+  //     console.log('true')
+  //   }
+  // }
+
+
+  // function isEmailValid(){   
+  //   const email = container.querySelector("#emailRegister").value;
+  //   if (!email){
+  //     return false;
+  //   }
+  //   return validateEmail(email);
+  // }
+
+   
 
   return container;
 };
