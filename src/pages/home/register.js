4058eb05eb6362d45ce718fc8ea2aebe014768bf
ThamaraBortolin <thamaralbortolin@gmail.com>
2022-04-27 17:21:32 -0300

    att firebase

--- src/pages/home/register.js
+++ src/pages/home/register.js
@@ -1,4 +1,4 @@
-import {userRegister} from "../firebase/auth-firebase.js";
+import "../firebase/config-firebase.js"
 
 export default () => {
   const container = document.createElement('div');
@@ -18,108 +18,56 @@ export default () => {
   <label for="check-Senha">Confirmar senha:</label>
   <input type="password" id="checkPassword" class="inputs-log" placeholder="Digite novamente sua senha" required></input>
   
-<<<<<<< HEAD
   <button class="button-register-cas" id="buttonRegister" disabled="disabled" type="button">Cadastrar</button>
-=======
-  <button class="button-register-cas" id="buttonRegister" >Cadastrar</button>
->>>>>>> 4d5f0fbaf2b4130ea593b829fd4d869587a1eb33
   </form>
   `;
 
   container.innerHTML = template;
 
-  // const name = container.querySelector("#name");
-  const email = container.querySelector("#emailRegister");
-  const password = container.querySelector("#password");
-  // const checkPassword = container.querySelector("#checkSenha");*/
+   // const name = container.querySelector("#name");
+   const email = container.querySelector("#emailRegister");
+   const password = container.querySelector("#password");
+   // const checkPassword = container.querySelector("#checkSenha");*/
+ 
+   container.addEventListener("submit", (e) => {
+     e.preventDefault();
+     userRegister(email.value, password.value);
+ 
+   })
 
-  container.addEventListener("submit", (e) => {
-    e.preventDefault();
-    userRegister(email.value, password.value);
-
-  })
-
-<<<<<<< HEAD
-  const inputs = container.querySelector("#emailRegister");
-  const button = container.querySelector("#buttonRegister");
-  const email = container.querySelector("#emailRegister");
+  // const inputs = container.querySelector("#emailRegister");
+  // const button = container.querySelector("#buttonRegister");
+  // const email = container.querySelector("#emailRegister");
 
 
-  function checkInputs(inputs) {
-    const filled = true;
-    if (inputs.value === "") {
-      filled = false;
-    }
-    return filled;
-  };
+  // function checkInputs(inputs) {
+  //   const filled = true;
+  //   if (inputs.value === "") {
+  //     filled = false;
+  //   }
+  //   return filled;
+  // };
 
-  function validateEmail(email) {
-    var re = /\S+@\S+\.\S+/;
-    if(re.test(email)){
-      return false
-    }else{
-      return true
-    }
-   }
+  // function validateEmail(email) {
+  //   const re = /\S+@\S+\.\S+/;
+  //   if(re.test(email)){
+  //     return gitfalse
+  //   }else{
+  //     return true
+  //   }
+  //  }
 
-  inputs.addEventListener("change", function () {
-    if (checkInputs(inputs) && validateEmail(email)) {
-      button.disabled = false;    
-    } else {
-      button.disabled = true;
-  }})
+  // inputs.addEventListener("change", function () {
+  //   if (checkInputs(inputs) && validateEmail(email)) {
+  //     button.disabled = false;    
+  //   } else {
+  //     button.disabled = true;
+  // }})
 
   
-  // const inpEmail = container.querySelector("#emailRegister")
-  // inpEmail.addEventListener("keypress",emailBlockingButton())
-
-  // function emailBlockingButton() {
-  //   // const emailValid = isEmailValid();
-  //   // container.querySelector("#buttonRegister").disabled = !emailValid;
-  //   const email = container.querySelector("#emailRegister").value
-
-  //   if(email === ""){
-  //     container.querySelector("#buttonRegister").classList.add("false")
-  //     console.log('false')
-  //   }else{
-  //     container.querySelector("#buttonRegister").classList.add("true")
-  //     console.log('true')
-  //   }
-  // }
-
-
-  // function isEmailValid(){   
-  //   const email = container.querySelector("#emailRegister").value;
-  //   if (!email){
-  //     return false;
-  //   }
-  //   return validateEmail(email);
-  // }
+  
 
    
-=======
-
-
-
-
-//   function emailBlockingButton() {
-//     const email = container.querySelector("#emailRegister").value
-//     if (!email) {
-//       document.querySelector("#buttonRegister").disabled = true
-//     }else if (validateEmail(email)){
-//       document.querySelector("#buttonRegister").disabled = false
-//     }
-//     else{
-//       document.querySelector("#buttonRegister").disabled = true
-//     }
-//   }
-//   emailBlockingButton()
-
-//   function validateEmail(email) {
-//     var re = /\S+@\S+\.\S+/;
-//     return re.test(email);
-//   }
->>>>>>> 4d5f0fbaf2b4130ea593b829fd4d869587a1eb33
 
   return container;
 };
