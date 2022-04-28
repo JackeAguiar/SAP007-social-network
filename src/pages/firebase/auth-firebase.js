cfebaeb4dd11ee295d0b31bca1594470cf913880
ThamaraBortolin <thamaralbortolin@gmail.com>
2022-04-28 20:06:08 -0300

    firebase cad e login

--- src/pages/firebase/auth-firebase.js
+++ src/pages/firebase/auth-firebase.js
@@ -1,21 +1,37 @@
-import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    GoogleAuthProvider,
+    signInWithPopup
+} from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
 
 const auth = getAuth();
+const provider = new GoogleAuthProvider();
 
 export function userRegister(email, password) {
- return createUserWithEmailAndPassword(auth, email, password)
+    return createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
-            // Signed in
             const user = userCredential.user;
-            // ...
-            console.log(user)
+            return user
         })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.log("errorCode", errorCode)
-            console.log("errorMsg", errorMessage)
-            // ..
-        });
+}
 
+export function userLogIn(email, password) {
+    return signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            return user
+        })
+}
+
+
+export function googleLogIn() {
+    return signInWithPopup(auth, provider)
+        .then((result) => {
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            // const token = credential.accessToken;
+            // const user = result.user;
+            return credential
+        })
 }
--- src/pages/home/register.js
+++ src/pages/home/register.js
@@ -1,4 +1,6 @@
-import "../firebase/config-firebase.js"
+import {
+  userRegister
+} from "../firebase/auth-firebase.js";
 
 export default () => {
   const container = document.createElement('div');
@@ -8,66 +10,81 @@ export default () => {
   <form>
   <label for="nome">Nome:</label>
   <input type="text" id="name" class="inputs-log" placeholder="Nome de usuário" required></input>
-
   <label for="e-mail">E-mail:</label>
-  <input type"email" id="emailRegister" class="inputs-log" placeholder="exemplo@gmail.com" required></input>
-
+  <input type"email" id="emailRegister" onchance="emailBlockingButton()" class="inputs-log" placeholder="exemplo@gmail.com" required></input>
   <label for="senha">Senha:</label>
   <input type="password" id="password" class="inputs-log" placeholder="Digite uma senha de 6 a 8 dígitos" required></input>
-
   <label for="check-Senha">Confirmar senha:</label>
   <input type="password" id="checkPassword" class="inputs-log" placeholder="Digite novamente sua senha" required></input>
-  
-  <button class="button-register-cas" id="buttonRegister" disabled="disabled" type="button">Cadastrar</button>
+  <p id="erro"></p>
+  <a href="/#feed">
+  <button class="button-register-cas" id="buttonRegister" >Cadastrar</button>
+  </a>
   </form>
   `;
 
   container.innerHTML = template;
 
-   // const name = container.querySelector("#name");
-   const email = container.querySelector("#emailRegister");
-   const password = container.querySelector("#password");
-   // const checkPassword = container.querySelector("#checkSenha");*/
- 
-   container.addEventListener("submit", (e) => {
-     e.preventDefault();
-     userRegister(email.value, password.value);
- 
-   })
+  // const name = container.querySelector("#name");
+  const email = container.querySelector("#emailRegister");
+  const password = container.querySelector("#password");
+  const erroMsg = container.querySelector("#erro")
+  const checkPassword = container.querySelector("#checkPassword");
+  // const btnRegister = container.querySelector("#buttonRegister")
 
-  // const inputs = container.querySelector("#emailRegister");
-  // const button = container.querySelector("#buttonRegister");
-  // const email = container.querySelector("#emailRegister");
+  container.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (password.value == checkPassword.value) {
+      userRegister(email.value, password.value)
+        .then(() => {
+          window.location.hash = "#feed";
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          erroMsg.classList.add("error")
+          switch (errorCode) {
+            case "auth/email-already-in-use":
+              erroMsg.innerHTML = "Este email já está em uso"
+              break;
+            case "auth/invalid-email":
+              erroMsg.innerHTML = "Email inválido"
+              break;
+            case "auth/weak-password":
+              erroMsg.innerHTML = "Digite uma senha com no mínimo 6 caracteres"
+              break;
+            default:
+              erroMsg.innerHTML = "Não foi possível realizar o cadastro"
+          }
+          const errorMessage = error.message
+          return errorMessage
+        });
+    } else {
+      erroMsg.innerHTML = "As senhas precisam ser iguais"
+    }
+
+  });
 
 
-  // function checkInputs(inputs) {
-  //   const filled = true;
-  //   if (inputs.value === "") {
-  //     filled = false;
+
+
+
+  //   function emailBlockingButton() {
+  //     const email = container.querySelector("#emailRegister").value
+  //     if (!email) {
+  //       document.querySelector("#buttonRegister").disabled = true
+  //     }else if (validateEmail(email)){
+  //       document.querySelector("#buttonRegister").disabled = false
+  //     }
+  //     else{
+  //       document.querySelector("#buttonRegister").disabled = true
+  //     }
   //   }
-  //   return filled;
-  // };
+  //   emailBlockingButton()
 
-  // function validateEmail(email) {
-  //   const re = /\S+@\S+\.\S+/;
-  //   if(re.test(email)){
-  //     return gitfalse
-  //   }else{
-  //     return true
+  //   function validateEmail(email) {
+  //     var re = /\S+@\S+\.\S+/;
+  //     return re.test(email);
   //   }
-  //  }
-
-  // inputs.addEventListener("change", function () {
-  //   if (checkInputs(inputs) && validateEmail(email)) {
-  //     button.disabled = false;    
-  //   } else {
-  //     button.disabled = true;
-  // }})
-
-  
-  
-
-   
 
   return container;
 };
